869913103f17f53c71a4229defe2df7215d71968
gg-mg <ggichuki2017@gmail.com>
2020-01-10 03:00:49 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+$name = "James";
+echo "<h1>This is Header 1</h1>"; 
+echo "<h2>This is Header 1</h2>"; 
+
+?>
+
+
+<?php if($name = "James") { ?>
+<p>Hello James!</p>
+<?php } else { ?>
+<p>You're not James.</p>
+<?php } ?>
