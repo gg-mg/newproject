aea9cf4e786ed95f1ea4d57f46c48bcd4e809399
gg-mg <ggichuki2017@gmail.com>
2020-01-14 10:18:16 -0600

    Added the for loop to newproject/index.html

--- index.php
+++ index.php
@@ -12,3 +12,17 @@ echo "<h2>This is Header 1</h2>";
 <p>You're not James.</p>
 <?php } ?>
 
+<?php
+
+for($counter=1; $counter<=5; $counter++){
+   $message = $message.$counter."|";
+}
+echo $message;
+?>
+<?php
+echo "<br>";
+for($counter=10; $counter<=100; $counter+=10){
+    echo $counter."<br>";
+}
+
+?>
